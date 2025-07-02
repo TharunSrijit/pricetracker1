f21ae80e9725beff96cb9e534c5bd7573619181f
TharunSrijit <tharungarena@gmail.com>
2025-07-02 23:07:04 +0530

    initial commit

---
+++ Components/Searchbar.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { scrapeAndStoreProduct } from "@/lib/actions";
+import { FormEvent, useState } from "react"
+
+const isValidAmazonProductURL=(url:string) =>{
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if(hostname.includes("amazon.in")||hostname.includes("amazon.")||
+    hostname.includes("amazon"))
+    {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+}
+
+const searchbar = () => {
+  const [searchPrompt, setsearchPrompt] = useState('');
+  const [isLoading, setisLoading] = useState(false);
+    const handleSubmit= async(event: FormEvent<HTMLFormElement>) =>{
+      event.preventDefault();
+
+      const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+      alert(isValidLink ? 'Valid link' : 'Invalid link')
+      if (!isValidLink) return alert('Please providea valid amazone link')
+        
+        try {
+            setisLoading(true);
+            //Scrape the product
+            const product = await scrapeAndStoreProduct
+        } catch (error) {
+          console.log(error);
+          
+        }finally{
+          setisLoading(false);
+        }
+      
+    }
+  return (
+     <form 
+      className="flex flex-wrap gap-4 mt-12" 
+      onSubmit={handleSubmit}
+    >
+      <input 
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setsearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+
+      <button 
+        type="submit" 
+        className="searchbar-btn"
+        disabled={searchPrompt===''}
+      >
+        {isLoading ? 'Searching' : 'Search'}
+      </button>
+    </form>
+  )
+}
+
+export default searchbar
---
+++ lib/scrapper/index.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export async function scrapeAmazonProduct(url:string) {
+    if(!url) return;
+
+    
+
+    //Brightdata proxy config
+    const username =String(process.env.BRIGHT_DATA_USERNAME);
+    const password =String(process.env.BRIGHT_DATA_PASSWORD);
+    const port = 33335;
+    const session_id = (1000000 * Math.random())|0;
+    const options = {
+    auth: {
+        username: `${username}-session-${session_id}`,
+        password,
+    },
+
+    host:'brd.superproxy.io',
+    port,
+    rejectUnauthorized: false,
+    }
+
+    try {
+        //fetch the product page
+        const response = await axios.get(url,options);
+        
+        console.log(response.data); 
+    } catch (error:any) {
+        throw new Error(`Failed to scrape product:${error.message}`)
+    }
+    
+}
