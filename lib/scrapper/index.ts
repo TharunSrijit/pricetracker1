f01ffddbc460f21ff586fc2a34dac7b59a01752f
TharunSrijit <tharungarena@gmail.com>
2025-07-07 19:10:06 +0530

    scrapped price,current price and image of product from amazon

--- lib/scrapper/index.ts
+++ lib/scrapper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url:string) {
     if(!url) return;
@@ -29,7 +30,25 @@ export async function scrapeAmazonProduct(url:string) {
         
         //extract product details
         const title=$('#productTitle').text().trim();
-        console.log({title})
+        const currentPrice = extractPrice(
+            $('.priceToPay span.a-price-whole'),
+            $('a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+        );
+
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('.a-price.a-text-price span.a-offscreen')
+        );
+
+        const outofStock = $('#availability span').text().trim().toLowerCase()==='currently unavailable';
+
+        const images = 
+            $('#imgBlkFront').attr('data-a-dynamic-image') || 
+            $('#landingImage').attr('data-a-dynamic-image') ||
+            '{}'
+
+        console.log({title,currentPrice, originalPrice})
     } catch (error:any) {
         throw new Error(`Failed to scrape product:${error.message}`)
     }
