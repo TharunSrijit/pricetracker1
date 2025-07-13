0d1c55d16c94a32cd39fd46b64670618a4ae44db
TharunSrijit <tharungarena@gmail.com>
2025-07-13 19:06:51 +0530

    completed the scrape now store left

---
+++ lib/mongoose.ts
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose'
+
+
+let isConnected = false;//variable for connection status
+
+export const connectToDB = async () =>{
+    
+}
--- lib/scrapper/index.ts
+++ lib/scrapper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractPrice } from "../utils";
+import { extractCurrency, extractPrice, extractDescription } from "../utils";
 
 export async function scrapeAmazonProduct(url:string) {
     if(!url) return;
@@ -45,10 +45,38 @@ export async function scrapeAmazonProduct(url:string) {
 
         const images = 
             $('#imgBlkFront').attr('data-a-dynamic-image') || 
-            $('#landingImage').attr('data-a-dynamic-image') ||
-            '{}'
+            $('#landingImage').attr('data-a-dynamic-image')||
+            '{}';
+        
+        const imageUrls=Object.keys(JSON.parse(images));
+        const currency = extractCurrency($('.a-price-symbol'))
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g,"");
 
-        console.log({title,currentPrice, originalPrice})
+        const description = extractDescription($)
+
+        //console.log({title,currentPrice, originalPrice, outofStock, imageUrls})
+
+        //Constuct data object with scrapped info
+        const data={
+            url,currency:currency || '$',
+            image: imageUrls[0],title,
+            currentPrice:Number(currentPrice)||Number(originalPrice),
+            originalPrice:Number(originalPrice)||Number(currentPrice),
+            priceHistory:[],
+            discountRate:Number(discountRate),
+            category:'category',
+            reviewsCount:100,
+            stars:4.5,
+            isOutOfStock:outofStock,
+            description,
+            lowestPrice:Number(currentPrice)||Number(originalPrice),
+            highestPrice:Number(originalPrice)||Number(currentPrice),
+            average:Number(currentPrice)||Number(originalPrice)
+        }
+
+        //console.log(data);
+        return data;
+
     } catch (error:any) {
         throw new Error(`Failed to scrape product:${error.message}`)
     }
