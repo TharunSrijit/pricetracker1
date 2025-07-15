404986ce540a4e33439bc4e168f1238a4634f165
TharunSrijit <tharungarena@gmail.com>
2025-07-15 22:44:04 +0530

    july15

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,5 +4,19 @@ import mongoose from 'mongoose'
 let isConnected = false;//variable for connection status
 
 export const connectToDB = async () =>{
-    
+    mongoose.set('strictQuery',true);
+
+    if(!process.env.MONGOBD_URI) return console.log('MONGODB_URI is not defined');
+
+    if(isConnected) return console.log('=> using existing database connection');
+
+    try {
+        await mongoose.connect(process.env.MONGOBD_URI,)
+
+        isConnected=true;
+
+        console.log('MONGODB Connected');
+    } catch (error) {
+        console.log(error)
+    }
 }
