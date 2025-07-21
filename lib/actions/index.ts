eb3cfa6533ac558d1c11c39c038e5dba87ad06d6
TharunSrijit <tharungarena@gmail.com>
2025-07-21 23:51:48 +0530

    AddedproductCard,a rotating carousel of products from DB

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scrapper";
@@ -32,9 +33,43 @@ export async function scrapeAndStoreProduct(productUrl:string) {
             }
         }
 
+        const newProduct = await Product.findOneAndUpdate({
+            url:scrapedProduct.url},
+            product,
+            {upsert:true,new:true}
+        );
+
+        revalidatePath(`/products/${newProduct._id}`);
+
 
     } catch (error:any) {
         throw new Error(`Failed to create/update product: ${error.message}`);
         
+    }
+}
+
+export async function getProductById(productId:string){
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({_id:productId});
+
+        if (!product) return null;
+
+        return product;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getAllProducts() {
+    try {
+        connectToDB();
+
+        const products = await Product.find();
+
+        return products;
+    } catch (error) {
+        console.log(error);
     }
 }
