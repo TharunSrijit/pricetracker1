7d729371317e1bfbbec0e8a58a88940657f6602f
TharunSrijit <tharungarena@gmail.com>
2025-08-05 23:34:38 +0530

    Made the products page and got bored

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { getProductById } from "@/lib/actions"
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const ProductDetails = () => {
+type Props={
+  params:{id:string}
+}
+
+const ProductDetails = async ({params:{id}}: Props) => {
+const product = await getProductById(id);
+
+if(!product) redirect('/')
+
   return (
-    <div>ProductDetails</div>
+    <div className="product-container">
+      <div className="flex gap-28 xl:flex-row flex-col">
+        <div className="product-image">
+          <Image
+          src={product.image}
+          alt={product.title}
+          width={500}
+          height={400}
+          className="mx-auto"
+          />
+        </div>
+        <div className="flex-1 flex flex-col">
+          <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
+            <div className="flex flex-col gap-3">
+              <p className="text-[28px] text-secondary font-semibold">{product.title}</p>
+              <Link 
+              href={product.url}
+              target="_blank"
+              className="text-base text-black opacity-50"
+              >
+                Visit Product
+              </Link>
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+      </div>
   )
 }
 
